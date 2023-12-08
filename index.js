026517e5587d4214e0ca13dc7981cb9e748b291d
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2023-12-08 09:34:42 +0100

    eleven

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 let BASE_URL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com';
 let planetInfo;
 
-//hämtar planeterna från html index
-let solen = document.getElementById('ellipse1');
-let mercury = document.getElementById('ellispe2');
-let venus = document.getElementById('ellipse3');
-let jorden = document.getElementById('ellipse4');
-let march = document.getElementById('ellipse5');
-let jupiter = document.getElementById('ellipse6');
-let saturn = document.getElementById('ellipse7');
-let uranus = document.getElementById('ellipse8');
-let neptunus = document.getElementById('ellipse9');
-
 //hämtar apinyckeln
 async function getKey(){
   let resp = await fetch(`${BASE_URL}/keys`, {method: 'POST'})
@@ -28,21 +17,24 @@ async function solarisBodies(apiKey){
     console.log(data)
     bodies(data.bodies)
 }
-
+//loopar igenom datan om himlakropparna och skriver ut dem i en ul (planetList)
 function bodies(data){
 let planetList = document.getElementById('planetlist')
    data.forEach((planet)=>{
     let listPlanet = document.createElement('li');
+    //knapp som kopplas till varje li för att få fram info om planeten (displayPlanetInfo) samt tangent tryck för att stänga info fönstret
     let button = document.createElement('button');
     button.addEventListener('click',()=> displayPlanetInfo(planet))
     button.addEventListener('keydown', resetpage)
+    //text innehåll samt sammankoppling av delar från html och js (planetList ul, listPlanet li, button)
     listPlanet.textContent = planet.name
     listPlanet.append(button)
+    //försök till att sätta klass namn 0 och uppåt enligt listans längd
     button.setAttribute('class', 'listplanet.length = 0++')
     planetList.append(listPlanet)
    });
 }
-
+//hämtar utvald info om planet och skriver ut det i html taggar inom html div (planetinfo)
 function displayPlanetInfo(planet){
     let planetInfo = document.getElementById('planetinfo')
     planetInfo.innerHTML = `
@@ -58,13 +50,13 @@ function displayPlanetInfo(planet){
     `
 }
 
-
+//funktion som innehåller dvs funktioner som skall startas när start sidan öppnas
 async function startPage(){
     const apiKey = await getKey()
     planetInfo= await solarisBodies(apiKey)
     console.log(planetInfo)
 }
-
+//funktion för att återställa startstidan vid tangent tryck efter visad info om planet
 const resetpage = () => {
     location.reload()
 }
